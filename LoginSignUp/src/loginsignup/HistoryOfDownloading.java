ab70ca5e5a74b64eadc42d59f1776f93a256eccf
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-11-09 17:12:22 +0200

    action of log out button in history of downloadings

--- LoginSignUp/src/loginsignup/HistoryOfDownloading.java
+++ LoginSignUp/src/loginsignup/HistoryOfDownloading.java
@@ -13,7 +13,7 @@ public class HistoryOfDownloading extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         ComeBack = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
+        LogOutBtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("HistoryOfDownl;oading");
@@ -33,9 +33,14 @@ public class HistoryOfDownloading extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setBackground(new java.awt.Color(204, 204, 255));
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jButton1.setText("Log out ...");
+        LogOutBtn.setBackground(new java.awt.Color(204, 204, 255));
+        LogOutBtn.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        LogOutBtn.setText("Log out ...");
+        LogOutBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                LogOutBtnActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -44,7 +49,7 @@ public class HistoryOfDownloading extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(43, 43, 43)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LogOutBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1)
                     .addComponent(ComeBack))
                 .addContainerGap(323, Short.MAX_VALUE))
@@ -57,7 +62,7 @@ public class HistoryOfDownloading extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 368, Short.MAX_VALUE)
                 .addComponent(ComeBack)
                 .addGap(18, 18, 18)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(LogOutBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(24, 24, 24))
         );
 
@@ -85,6 +90,13 @@ public class HistoryOfDownloading extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_ComeBackActionPerformed
 
+    private void LogOutBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LogOutBtnActionPerformed
+       Login LoginFrame = new Login();
+       LoginFrame.setVisible(true);
+       LoginFrame.pack();
+       LoginFrame.setLocationRelativeTo(null);
+    }//GEN-LAST:event_LogOutBtnActionPerformed
+
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -95,7 +107,7 @@ public class HistoryOfDownloading extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton ComeBack;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton LogOutBtn;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
