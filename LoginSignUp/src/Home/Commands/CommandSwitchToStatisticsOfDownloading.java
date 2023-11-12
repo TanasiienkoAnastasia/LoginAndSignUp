25ed33641f35d09719b413b3d73d5ee52b70796c
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-11-12 23:23:47 +0200

    switchToStatisticsOfDownloading command

--- LoginSignUp/src/Home/Commands/CommandSwitchToStatisticsOfDownloading.java
+++ LoginSignUp/src/Home/Commands/CommandSwitchToStatisticsOfDownloading.java
@@ -1,5 +1,12 @@
 package Home.Commands;
 
+import StatisticsOfDownloading.StatisticsOfDownloading;
+
 public class CommandSwitchToStatisticsOfDownloading {
-    
+    public void switchToStatisticsOfDownloading(){
+        StatisticsOfDownloading DateStatisticsOfDownloadingFrame = new StatisticsOfDownloading();
+        DateStatisticsOfDownloadingFrame.setVisible(true);
+        DateStatisticsOfDownloadingFrame.pack();
+        DateStatisticsOfDownloadingFrame.setLocationRelativeTo(null); 
+    }
 }
