0ddc5c3852895cee88b815665439430c2c4db2ed
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-11-12 23:19:38 +0200

    switchToStartDownloadingPage command

--- LoginSignUp/src/Home/Commands/CommandSwitchToStartDownloadingPage.java
+++ LoginSignUp/src/Home/Commands/CommandSwitchToStartDownloadingPage.java
@@ -1,5 +1,12 @@
 package Home.Commands;
 
+import DownloadManager.DownloadManager;
+
 public class CommandSwitchToStartDownloadingPage {
-    
+    public void switchToStartDownloadingPage(){
+        DownloadManager DownloadManagerFrame = new DownloadManager();
+        DownloadManagerFrame.setVisible(true);
+        DownloadManagerFrame.pack();
+        DownloadManagerFrame.setLocationRelativeTo(null);
+    }
 }
