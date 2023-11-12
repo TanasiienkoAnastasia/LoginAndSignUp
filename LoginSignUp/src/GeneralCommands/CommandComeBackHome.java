669005712a873f34b2dcf129fa9c849fc5ed51f1
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-11-12 23:10:46 +0200

    come back home command

--- LoginSignUp/src/GeneralCommands/CommandComeBackHome.java
+++ LoginSignUp/src/GeneralCommands/CommandComeBackHome.java
@@ -1,5 +1,12 @@
 package GeneralCommands;
 
+import Home.Home;
+
 public class CommandComeBackHome {
-    
+    public void comeBackHome(){
+        Home HomeFrame = new Home();
+        HomeFrame.setVisible(true);
+        HomeFrame.pack();
+        HomeFrame.setLocationRelativeTo(null); 
+    }
 }
