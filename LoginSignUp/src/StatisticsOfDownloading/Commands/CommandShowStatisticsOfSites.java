0b8f463c7b78b7fdc031879bbf8bf1109f064352
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-11-13 03:15:59 +0200

    create commandShowStatisticsOfSites

--- LoginSignUp/src/StatisticsOfDownloading/Commands/CommandShowStatisticsOfSites.java
+++ LoginSignUp/src/StatisticsOfDownloading/Commands/CommandShowStatisticsOfSites.java
@@ -1,7 +1,13 @@
 package StatisticsOfDownloading.Commands;
 
+import StatisticsOfDownloading.StatisticsOfSites;
+
+
 public class CommandShowStatisticsOfSites {
     public void commandShowStatisticsOfSites(){
-        
+        StatisticsOfSites StatisticsOfSitesFrame = new StatisticsOfSites();
+        StatisticsOfSitesFrame.setVisible(true);
+        StatisticsOfSitesFrame.pack();
+        StatisticsOfSitesFrame.setLocationRelativeTo(null);
     }
 }
