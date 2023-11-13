9e573af97f6b5ae014005f1c25f6af3132a00c2d
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-11-13 03:18:39 +0200

    create commandShowStatisticsOfVolume

--- LoginSignUp/src/StatisticsOfDownloading/Commands/CommandShowStatisticsOfVolume.java
+++ LoginSignUp/src/StatisticsOfDownloading/Commands/CommandShowStatisticsOfVolume.java
@@ -1,7 +1,13 @@
 package StatisticsOfDownloading.Commands;
 
+import StatisticsOfDownloading.StatisticsOfVolume;
+
+
 public class CommandShowStatisticsOfVolume {
     public void commandShowStatisticsOfVolume(){
-        
+        StatisticsOfVolume StatisticsOfVolumeFrame = new StatisticsOfVolume();
+        StatisticsOfVolumeFrame.setVisible(true);
+        StatisticsOfVolumeFrame.pack();
+        StatisticsOfVolumeFrame.setLocationRelativeTo(null);
     }
 }
