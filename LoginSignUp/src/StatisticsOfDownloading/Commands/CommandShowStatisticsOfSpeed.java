92176040eff759379ea78680ee39f8de2b01fde3
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-11-13 03:17:14 +0200

    create StatisticsOfSpeedFrame

--- LoginSignUp/src/StatisticsOfDownloading/Commands/CommandShowStatisticsOfSpeed.java
+++ LoginSignUp/src/StatisticsOfDownloading/Commands/CommandShowStatisticsOfSpeed.java
@@ -1,8 +1,14 @@
 package StatisticsOfDownloading.Commands;
 
+import StatisticsOfDownloading.StatisticsOfSites;
+import StatisticsOfDownloading.StatisticsOfSpeed;
+
 
 public class CommandShowStatisticsOfSpeed {
     public void commandShowStatisticsOfSpeed(){
-        
+        StatisticsOfSpeed StatisticsOfSpeedFrame = new StatisticsOfSpeed();
+        StatisticsOfSpeedFrame.setVisible(true);
+        StatisticsOfSpeedFrame.pack();
+        StatisticsOfSpeedFrame.setLocationRelativeTo(null);
     }
 }
