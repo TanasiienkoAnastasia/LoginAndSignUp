d3800dd6bfe9a98f8d6703036bb33cad3c073236
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-11-13 03:14:21 +0200

    create commandShowStatisticsOfDates

--- LoginSignUp/src/StatisticsOfDownloading/Commands/CommandShowStatisticsOfDates.java
+++ LoginSignUp/src/StatisticsOfDownloading/Commands/CommandShowStatisticsOfDates.java
@@ -1,7 +1,13 @@
 package StatisticsOfDownloading.Commands;
 
+import DownloadManager.DownloadManager;
+import StatisticsOfDownloading.StatisticsOfDates;
+
 public class CommandShowStatisticsOfDates {
     public void commandShowStatisticsOfDates(){
-        
+        StatisticsOfDates StatisticOfDatesFrame = new StatisticsOfDates();
+        StatisticOfDatesFrame.setVisible(true);
+        StatisticOfDatesFrame.pack();
+        StatisticOfDatesFrame.setLocationRelativeTo(null);
     }
 }
